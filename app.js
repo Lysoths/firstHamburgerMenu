c8bf45ee1721b683a50e967f900812e3ce5f8cbd
Oğuzhan ER <95685721+Lysoths@users.noreply.github.com>
2022-05-31 23:40:49 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const menu =  document.querySelector(".menu");
+const button = document.querySelector(".fa-bars");
+const headerContainer = document.querySelector(".header-container")
+console.log(menu.classList);
+
+
+button.addEventListener("click", ()=> {
+
+      menu.classList.toggle("asd");
+})
